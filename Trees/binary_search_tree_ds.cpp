94f3d05a7a8a3010abd99f27a269f1a4934c6a08
kartavya kothari <kartavya.kothari24@gmail.com>
2021-06-20 10:54:25 +0530

    Added code

--- Trees/binary_search_tree_ds.cpp
+++ Trees/binary_search_tree_ds.cpp
@@ -14,35 +14,148 @@ class Node{
 
 class BST{
     Node *root;
-    public:
-    BST(){
 
-    }
-
-    void insert_ele(int ele){}
-    bool is_present(int ele){}
-
-    void inorder(Node *root){
+    void inorder_traverse(Node *root){
         if(root == nullptr) return;
 
-        inorder(root->left);
+        inorder_traverse(root->left);
         cout<<root->data<<" ";
-        inorder(root->right);
+        inorder_traverse(root->right);
     }
-
-    void postorder(Node *root){
+    
+    void postorder_traverse(Node *root){
         if(root == nullptr) return;
         
-        postorder(root->left);
-        postorder(root->right);
+        postorder_traverse(root->left);
+        postorder_traverse(root->right);
         cout<<root->data<<" ";
     }
 
-    void preorder(Node *root){
+    void preorder_traverse(Node *root){
         if(root == nullptr) return;
 
         cout<<root->data<<" ";
-        preorder(root->left);
-        preorder(root->right);
+        preorder_traverse(root->left);
+        preorder_traverse(root->right);
     }
+
+    // Insersion rec method 1
+    // void insert_ele(Node *root,int ele){
+    //     if(root == nullptr){
+    //         root = new Node(ele);
+    //         return;
+    //     }
+
+    //     if(ele <= root->data){
+    //         if(root->left == nullptr){
+    //             root->left = new Node(ele);
+    //         }else insert_ele(root->left,ele);
+    //     }else{
+    //         if(root->right == nullptr){
+    //             root->right = new Node(ele);
+    //         }else insert_ele(root->right,ele);
+    //     }
+    // }
+
+    // Insertion rec method 2
+    Node* insert_ele(Node *root, int ele){
+        if(root == nullptr){
+            return new Node(ele);
+        }
+
+        if(ele <= root->data){
+            root->left = insert_ele(root->left,ele);
+        }else root->right = insert_ele(root->right,ele);
+
+        return root;
+    }
+
+    public:
+    BST(){
+        root = nullptr;
+    }
+
+    // Recursive insert M1
+    // void insert(int ele){
+    //     insert_ele(root,ele);
+    // }
+
+    // Recursive insert M2
+    void insert(int ele){
+        root = insert_ele(root,ele);
+    }
+
+    // Iterative code for inserting
+    // void insert_ele(int ele){
+    //     Node *n = new Node(ele);
+    //     Node *p = root;
+
+    //     if(p==nullptr){
+    //         root = n;
+    //         return;
+    //     }
+
+    //     while(p!=nullptr){
+    //         if(ele <= p->data){
+    //             if(p->left == nullptr){
+    //                 p->left = n;
+    //                 return;
+    //             }
+    //             p = p->left;
+    //         }else{
+    //             if(p->right == nullptr){
+    //                 p->right = n;
+    //                 return;
+    //             }
+    //             p = p->right;
+    //         }
+    //     }
+    // }
+
+    bool is_present(int ele){}
+
+    void inorder(){
+        if(root==nullptr){
+            cout<<"Tree empty"<<endl;
+            return;
+        }
+        cout<<"Inorder: ";
+        inorder_traverse(root);
+        cout<<endl;
+    }
+
+    void preorder(){
+        if(root==nullptr){
+            cout<<"Tree empty"<<endl;
+            return;
+        }
+        cout<<"Preorder: ";
+        preorder_traverse(root);
+        cout<<endl;
+    }
+
+    void postorder(){
+        if(root==nullptr){
+            cout<<"Tree empty"<<endl;
+            return;
+        }
+        cout<<"Postorder: ";
+        postorder_traverse(root);
+        cout<<endl;
+    }
+};
+
+int main(){
+    BST tree;
+    vector<int> arr = {56,33,24,56,98,11,45,69,6,23,45,77,43};
+
+    for(int ele : arr){
+        tree.insert_ele(ele);
+    }
+
+    tree.inorder();
+    tree.preorder();
+    tree.postorder();
+
+    return 0;
 }
